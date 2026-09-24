0c21871cd46612c19b623b8b9d191a40aca5f393
stephane-gabon <58512435+stephane-gabon@users.noreply.github.com>
2026-09-25 00:39:02 +0100

    Update __init__.py

--- p4a-recipes/reportlab/__init__.py
+++ p4a-recipes/reportlab/__init__.py
@@ -1,9 +1,13 @@
+from pathlib import Path
+
 from pythonforandroid.recipe import Recipe
 
 
 class ReportLabRecipe(Recipe):
     version = "fe660f227cac"
-    url = "file:///home/runner/.buildozer/reportlab-fe660f227cac.tar.gz"
+
+    _recipe_dir = Path(__file__).resolve().parent
+    url = f"file://{_recipe_dir / 'reportlab-fe660f227cac.tar.gz'}"
 
     depends = ["python3"]
 
