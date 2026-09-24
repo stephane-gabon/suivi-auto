7773cd69f65d162f2cd19f730799b32b55b01b77
stephane-gabon <58512435+stephane-gabon@users.noreply.github.com>
2026-09-24 09:30:50 +0100

    Update __init__.py

---
+++ recipes/reportlab/__init__.py
@@ -0,0 +1,18 @@
+from pythonforandroid.recipe import PythonRecipe
+
+class ReportLabRecipe(PythonRecipe):
+version = "4.4.7"
+
+
+url = (
+    "https://files.pythonhosted.org/packages/source/r/"
+    "reportlab/reportlab-{version}.tar.gz"
+)
+
+depends = ["python3", "freetype"]
+
+site_packages_name = "reportlab"
+
+call_hostpython_via_targetpython = False
+
+recipe = ReportLabRecipe()
