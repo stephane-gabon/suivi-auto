b920013c70cc2231edd65df62e8ffd65baa3833e
stephane-gabon <58512435+stephane-gabon@users.noreply.github.com>
2026-09-25 01:12:39 +0100

    Update __init__.py

--- p4a-recipes/reportlab/__init__.py
+++ p4a-recipes/reportlab/__init__.py
@@ -7,6 +7,7 @@ class ReportLabRecipe(Recipe):
     version = "fe660f227cac"
 
     _recipe_dir = Path(__file__).resolve().parent
+
     url = f"file://{_recipe_dir / 'reportlab-fe660f227cac.tar.gz'}"
 
     depends = ["python3"]
